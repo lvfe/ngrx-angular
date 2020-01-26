f772b200401f97770319387bc6567604785ea390
fengjiao <lvfe@tcd.ie>
2020-01-26 11:31:48 +0800

    issue2：click event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, interval} from 'rxjs';
-import { map } from 'rxjs/operators';
-import { DatePipe } from '@angular/common';
-import { AsyncPipe } from '@angular/common';
+import { Observable, interval, Subject, merge} from 'rxjs';
+import { mapTo, scan, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +9,24 @@ import { AsyncPipe } from '@angular/common';
 })
 export class HomeComponent implements OnInit {
   private clock: Observable<Date>;
+  private update$ = new Subject();
 
   constructor() { }
 
   ngOnInit() {
-    this.clock = interval(5000).pipe(map( trd => new Date())); 
+    // mapTo is great to change cur, arr is save last change value
+    this.clock = merge(interval(1000).pipe(mapTo('second')), this.update$.asObservable().pipe(mapTo('hour'))).pipe(
+      startWith('2017/12/11'),
+      scan((arr, cur)=> {
+        console.log(arr,cur);
+        const arrDate = new Date(''+arr);
+        if(cur=='hour')
+          arrDate.setHours(arrDate.getHours()+1);
+        else
+          arrDate.setSeconds(arrDate.getSeconds()+1);
+        return arrDate;
+      })
+    ) as Observable<Date>; 
   }
 
 }
